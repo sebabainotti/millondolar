87916bdcdf5afe99274b824a54a0b83c5db0be0c
Sebastian Bainotti <sebabainotti@hotmail.com>
2023-07-31 17:27:02 +0200

    fg

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const [percentage, setPercentage] = useState(0);
   const [percentageClass, setPercentageClass] = useState('');
   const [change, setChange] = useState(false);
-  const gancho = 155576;
+  const gancho = 0;
   const [showDisclaimer, setShowDisclaimer] = useState(false);
 
   const handleShowDisclaimer = () => {
